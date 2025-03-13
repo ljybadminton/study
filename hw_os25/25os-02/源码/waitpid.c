20a4d8b28abf44b222bfbea69e4a2014af2468bf
ljybadminton <147171353+ljybadminton@users.noreply.github.com>
2025-03-13 21:00:33 +0800

    Add files via upload

---
+++ hw_os25/25os-02/源码/waitpid.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+int main(){
+    pid_t child_pid;
+    child_pid = fork();
+    if(child_pid < 0){
+        //Fork filed
+        perror("Fork failed");
+        return 1;
+    }else if(child_pid == 0){
+        printf("Child process: My PID is %d\n", getpid());
+    }
+    else{
+        //Code executed by parent process
+        printf("Parent process: Child process id is %d\n", child_pid);
+        //Wait for child process to complete
+        int status;
+        waitpid(child_pid, &status, 0);
+        if(WIFEXITED(status)){
+            printf("Child process exited with status: %d\n", WEXITSTATUS(status));
+        }
+    }
+    return 0;
+}
