3b988d260878d5968b6bb7ea2bda6c08403d1515
study <3139069904@qq.com>
2025-04-09 20:41:49 +0800

    update

---
+++ hw_os25/25os-03/hello.c
@@ -0,0 +1,18 @@
+#include<pthread.h>
+#include<stdio.h>
+#include<stdlib.h>
+void *worker(void *arg){
+	printf("Hello World!\n");
+	return (NULL);
+}
+int main(int argc,char *argv[]){
+	pthread_t tid;
+	if(pthread_create(&tid,NULL,worker,NULL)){
+		printf("Cannot create thread\n");
+		exit(1);
+	}
+	printf("Main waits for thread to finish....\n");
+	pthread_join(tid,NULL);
+	exit(0);
+}
+	
